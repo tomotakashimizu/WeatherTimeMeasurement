6987fb3d75f3dc27930a41b07731c4b994aa9a6b
tomotakashimizu <tomotaka.developer@gmail.com>
2021-08-09 07:59:33 +0900

    [update] weatherDescription (現在の天気), weatherCity (気象情報を取得するcity) を出力

--- src/measurement/Main.java
+++ src/measurement/Main.java
@@ -1,5 +1,8 @@
 package measurement;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class Main {
     
     public static void main(String[] args) {
@@ -7,8 +10,27 @@ public class Main {
                                             "?q=tokyo&units=metric",
                                             APIKey.getMyAPIKey(),
                                             "&lang=ja");
+        
         String weatherJsonString = openWeatherAPI.createJsonString();
+        System.out.println("\n######### weatherJsonString #########");
         System.out.println(weatherJsonString);
+        
+        JSONObject weatherJson = new JSONObject(weatherJsonString);
+        System.out.println("\n######### weatherJson #########");
+        System.out.println(weatherJson);
+
+        String weatherInfo = weatherJson.get("weather").toString();
+        System.out.println("\n######### weatherInfo #########");
+        System.out.println(weatherInfo);
+        
+        JSONArray weatherInfoJsonArray = new JSONArray(weatherInfo);
+        String weatherDescription = weatherInfoJsonArray.getJSONObject(0).get("description").toString();
+        System.out.println("\n######### weatherDescription #########");
+        System.out.println(weatherDescription);
+
+        String weatherCity = weatherJson.get("name").toString();
+        System.out.println("\n######### weatherCity #########");
+        System.out.println(weatherCity);
     }
     
 }
