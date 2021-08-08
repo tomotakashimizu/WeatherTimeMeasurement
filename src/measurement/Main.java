f8161c52f105de0c316f1e6fd13f1a151680d13d
tomotakashimizu <tomotaka.developer@gmail.com>
2021-08-09 07:42:33 +0900

    [update] 東京の現在の気象情報を日本語で出力, WebAPI クラスを更新

--- src/measurement/Main.java
+++ src/measurement/Main.java
@@ -4,8 +4,9 @@ public class Main {
     
     public static void main(String[] args) {
         WebAPI openWeatherAPI = new WebAPI("https://api.openweathermap.org/data/2.5/weather",
-                                            "?q=chiba&units=metric",
-                                            APIKey.getMyAPIKey());
+                                            "?q=tokyo&units=metric",
+                                            APIKey.getMyAPIKey(),
+                                            "&lang=ja");
         String weatherJsonString = openWeatherAPI.createJsonString();
         System.out.println(weatherJsonString);
     }
--- src/measurement/WebAPI.java
+++ src/measurement/WebAPI.java
@@ -9,36 +9,21 @@ import java.net.URLConnection;
 public class WebAPI {
     private String stringUrl;
     private String param;
-    private String apiKey = "";
+    private String apiKey;
+    private String lang = "";
 
-    WebAPI(String stringUrl, String param) {
+    WebAPI(String stringUrl, String param, String apiKey, String lang) {
         this.stringUrl = stringUrl;
         this.param = param;
-    }
-
-    WebAPI(String stringUrl, String param, String apiKey) {
-        this(stringUrl, param);
         this.apiKey = apiKey;
-    }
-    
-    public String getStringUrl() {
-        return stringUrl;
-    }
-    public String getParam() {
-        return param;
-    }
-    public void setStringUrl(String stringUrl) {
-        this.stringUrl = stringUrl;
-    }
-    public void setParam(String param) {
-        this.param = param;
+        this.lang = lang;
     }
     
     public String createJsonString() {
         String jsonData = "empty";
         URL url;
         try {
-            String completeStringUrl = this.stringUrl + this.param + "&appid=" + this.apiKey;
+            String completeStringUrl = this.stringUrl + this.param + "&appid=" + this.apiKey + this.lang;
             url = new URL(completeStringUrl);
             URLConnection urlConnection;
             urlConnection = url.openConnection();
