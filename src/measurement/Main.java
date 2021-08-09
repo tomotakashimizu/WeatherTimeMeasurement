41d7e75bdf7723c664ba9aacc619bf04b334dbcb
tomotakashimizu <tomotaka.developer@gmail.com>
2021-08-10 05:46:29 +0900

    [clean] 不要なコメントアウトを削除

--- src/measurement/Main.java
+++ src/measurement/Main.java
@@ -5,12 +5,8 @@ import java.util.Timer;
 public class Main {
     
     public static void main(String[] args) {
-        // Timerクラスのオブジェクトを作成
         Timer time = new Timer();
-
         System.out.println("実行開始");
-
-        // 一定間隔で処理を開始する
         // TimerTaskCallを、3秒後に、5秒間隔で実行する
         time.scheduleAtFixedRate(new TimerTaskCall(), 3000, 5000);
     }
