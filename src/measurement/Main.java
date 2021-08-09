9ff9561752131c5895e8e853eea2cd0312e42e42
tomotakashimizu <tomotaka.developer@gmail.com>
2021-08-10 05:41:12 +0900

    [update] TimerTask を用いて 5秒おきにAPIを叩く処理を追記

--- src/measurement/Main.java
+++ src/measurement/Main.java
@@ -1,43 +1,18 @@
 package measurement;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
+import java.util.Timer;
 
 public class Main {
     
     public static void main(String[] args) {
-        WebAPI openWeatherAPI = new WebAPI("https://api.openweathermap.org/data/2.5/weather",
-                                            "?q=tokyo&units=metric",
-                                            APIKey.getMyAPIKey(),
-                                            "&lang=ja");
-        
-        JSONObject weatherJson = openWeatherAPI.createJSON();
-        System.out.println("\n=== weatherJson ===");
-        System.out.println(weatherJson);
+        // Timerクラスのオブジェクトを作成
+        Timer time = new Timer();
 
-        String weatherInfo = weatherJson.get("weather").toString();
-        System.out.println("\n=== weatherInfo ===");
-        System.out.println(weatherInfo);
-        
-        JSONArray weatherInfoJsonArray = new JSONArray(weatherInfo);
-        String weatherDescription = weatherInfoJsonArray.getJSONObject(0).get("description").toString();
-        System.out.println("\n=== weatherDescription ===");
-        System.out.println(weatherDescription);
+        System.out.println("実行開始");
 
-        String weatherCity = weatherJson.get("name").toString();
-        System.out.println("\n=== weatherCity ===");
-        System.out.println(weatherCity);
-
-        // 現在日時情報で初期化されたインスタンスの取得
-        LocalDateTime nowDateTime = LocalDateTime.now(); 
-        DateTimeFormatter javaFormat = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-        // 日時情報を指定フォーマットの文字列で取得
-        String nowTime = nowDateTime.format(javaFormat);
-        System.out.println("\n=== 現在時刻 ===");
-        System.out.println(nowTime);
+        // 一定間隔で処理を開始する
+        // TimerTaskCallを、3秒後に、5秒間隔で実行する
+        time.scheduleAtFixedRate(new TimerTaskCall(), 3000, 5000);
     }
     
 }
