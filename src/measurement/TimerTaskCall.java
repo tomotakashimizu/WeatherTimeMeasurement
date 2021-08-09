1644fca0aab1b6933d3a478ca54ca19864354d7a
tomotakashimizu <tomotaka.developer@gmail.com>
2021-08-10 04:58:09 +0900

    [add] TimerTask の継承クラスを作成

---
+++ src/measurement/TimerTaskCall.java
@@ -0,0 +1,13 @@
+package measurement;
+
+import java.util.TimerTask;
+
+public class TimerTaskCall extends TimerTask {
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
