287a7e59f7a45be13f844febaad737a04cddb165
tomotakashimizu <tomotaka.developer@gmail.com>
2021-08-17 22:23:55 +0900

    [update] Postgres クラスをインスタンス化
    正常に DB に保存できるかテストするため

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,63 +1,13 @@
 package main;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 // import java.util.Timer;
 
 public class Main {
     
     public static void main(String[] args) {
 
-        Connection conn = null;
-        Statement stmt = null;
-        ResultSet rset = null;
-
-        //接続文字列
-        String url = "jdbc:postgresql://127.0.0.1:5432/testdb";
-        String user = "testuser";
-        String password = "testpass";
-
-        try{
-            //PostgreSQLへ接続
-            conn = DriverManager.getConnection(url, user, password);
-
-            //自動コミットOFF
-            conn.setAutoCommit(false);
-
-            //SELECT文の実行
-            stmt = conn.createStatement();
-            String sql = "SELECT 1";
-            rset = stmt.executeQuery(sql);
-
-            //SELECT結果の受け取り
-            while(rset.next()){
-                String col = rset.getString(1);
-                System.out.println(col);
-            }
-
-            //INSERT文の実行
-            sql = "INSERT INTO testtable VALUES (1, 'AAA')";
-            stmt.executeUpdate(sql);
-            conn.commit();
-        }
-        catch (SQLException e){
-            e.printStackTrace();
-        }
-        finally {
-            try {
-                if(rset != null)rset.close();
-                if(stmt != null)stmt.close();
-                if(conn != null)conn.close();
-            }
-            catch (SQLException e){
-                e.printStackTrace();
-            }
-
-        }
-
+        Postgres postgresTest = new Postgres("testdb", "testuser", "testpass");
+        postgresTest.createValues("(2, 'B')");
 
         // Timer time = new Timer();
         // System.out.println("実行開始");
